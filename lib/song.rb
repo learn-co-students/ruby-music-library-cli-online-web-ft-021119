a4cabbdc6825f930baf15a57acf3d20303d418b9
Joan Lyness <joanrigdon@gmail.com>
2019-03-01 14:58:24 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,12 +22,6 @@ class Song
     @@all.clear
   end
 
-  def self.create(name)
-    instance = self.new(name)
-    @@all << instance
-    instance
-  end
-
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
@@ -38,12 +32,18 @@ class Song
     self.genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    self.all.find {|song| song.name == name}
+  def Song.create(name)
+      song = Song.new(name)
+      song.name = name
+      song
+    end
+
+  def Song.find_by_name(name)
+    @@all.find{|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create(name)
+     Song.find_by_name(name) || Song.create(name)
   end
 
 
