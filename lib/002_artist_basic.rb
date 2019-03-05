421afaff2fc06cf7b0e27de41597e3559fc2bbf5
github username <github email address>
2019-03-05 15:30:31 -0500

    Done.

--- lib/002_artist_basic.rb
+++ lib/002_artist_basic.rb
@@ -1,11 +1,21 @@
-class Artist
-  attr_accessor :name
-  attr_reader
-  @@all = []
+require_relative  "../lib/Concerns/findable"
 
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs, :genres
+  @@all = []
 
   def initialize(name)
   @name = name
+  @songs = []
+  #@songs << song
+  #@genres = genres
+  end
+
+  def genres
+    songs.map(&:genre).uniq
+    #songs.genres.map.uniq {|w| w}
   end
 
   def self.all
@@ -25,5 +35,20 @@ class Artist
     artist.save
     artist
   end
+
+#  def songs
+  #  self.songs # << song
+#artist.songs.map(&:song)
+#  end
+
+
+  def add_song(song)
+    #song.artist = self
+  song.artist = self unless song.artist #if value is not nil will be true.
+    #   # artist
+  songs.push song unless songs.include?(song)
+  end
+
+
 
 end
--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,1 +1,37 @@
 require 'pry'
+
+#path
+#=> "./spec/fixtures/mp3s"
+
+class MusicImporter
+
+    #attr_accessor :path #, :files
+    attr_reader :path
+
+    def initialize(path)
+        @path = path
+    end
+     
+  
+
+    def files
+
+Dir.glob("#{path}/*").map{ |file| file.gsub("#{path}/", '')}
+
+          #Dir.glob("#{path}")
+    #Dir.glob("#{path}/*")
+#=> ["./spec/fixtures/mp3s/Jurassic 5 - What's Golden - hip-hop.mp3",
+#    "./spec/fixtures/mp3s/Real Estate - It's Real - hip-hop.mp3",
+#    "./spec/fixtures/mp3s/Real Estate - Green Aisles - country.mp3",
+#    "./spec/fixtures/mp3s/Thundercat - For Love I Come - dance.mp3",
+#    "./spec/fixtures/mp3s/Action Bronson - Larry Csonka - indie.mp3"]
+    end
+
+    # def import
+    #     Song.create
+    # end
+
+    def import
+        files.each{ |f| Song.create_from_filename(f) }
+    end
+end
