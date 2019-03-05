bd678a276fa85a35a8a46bcd367b35560bb76fca
Christoph Gruener <christophgruener@me.com>
2019-03-05 18:51:22 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,13 +38,44 @@ class MusicLibraryController
     # binding.pry
   end
 
+  # def list_artists
+  #   sorted_list = []
+  #   artist_names = (Song.all.collect {|s| s.artist.name}).sort
+  #   # binding.pry
+  #   artist_names.each do |artist|
+  #     # binding.pry
+  #     Song.all.each do |song|
+  #       # binding.pry
+  #       unless sorted_list.include?(song)
+  #         if song.artist.name == artist
+  #           sorted_list << song
+  #         end
+  #       end
+  #
+  #     end
+  #   end
+  #   sorted_list.each_with_index do |song, index|
+  #     # binding.pry
+  #     puts "#{index+1}. #{song.artist.name}"
+  #   end
+  #
+  # end
+
   def list_artists
+
     # binding.pry
     artists = Artist.all.collect {|a| a.name}.sort
     # binding.pry
     artists.each_with_index {|a, i| puts "#{i+1}. #{a}"}
 
   end
+
+    artists = Artist.all.collect {|s| s.name}
+    artists.uniq!
+    artists.sort!
+    # binding.pry
+    artists.each_with_index {|a, i| puts "#{i+1}. #{a}"}
+
 
   def list_genres
     genres = Genre.all.collect {|g| g.name}.sort
@@ -58,12 +89,13 @@ class MusicLibraryController
     artist_songs = []
     if Artist.all.collect {|a| a.name}.include?(gets)
       binding.pry
-      
+
 
     end
 
 
   end
+
 
 
 
