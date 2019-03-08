efd19080af9dcda52f1c278c514a78b2ea49c9c0
Alicia Santiago <albrown07@mac.com>
2019-03-08 15:23:00 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -34,6 +34,14 @@ attr_accessor :name, :songs, :genre
     self.songs.collect{|song| song.genre}.uniq
   end
 
+  def self.find_by_name(name)
+    self.all.detect{|x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
   def self.destroy_all
       @@all.clear
   end
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,10 @@
 module Concerns::Findable
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     self.all.detect{|x| x.name == name}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
+  def find_or_create_by_name(name)
+    find_by_name(name) || create(name)
   end
 end
