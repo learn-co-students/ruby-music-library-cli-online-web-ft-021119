e30de55bdbdcc1d9eca0bced4ebd40f77b404064
Alicia Santiago <albrown07@mac.com>
2019-03-10 15:54:35 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,7 +41,7 @@ extend Concerns::Findable
   def list_songs
     Song.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, i|
       puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    #  binding.pry
+      binding.pry
     end
   end
 
