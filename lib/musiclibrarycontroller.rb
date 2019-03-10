e05a32eed240f34363240d305c355578a97a37aa
Alicia Santiago <albrown07@mac.com>
2019-03-10 17:02:00 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -84,5 +84,6 @@ extend Concerns::Findable
       if (1..Song.all.length).include?(input)
       song = Song.all.sort {|a, b| a.name <=> b.name}[input - 1]
       end
-      puts "Playing #{song.name} by #{song.artist.name}"
-end
+      puts "Playing #{song.name} by #{song.artist.name}" if song
+    end
+  end
