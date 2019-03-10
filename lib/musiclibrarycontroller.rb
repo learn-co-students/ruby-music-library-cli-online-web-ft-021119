07c9e0bbe0adc8c2b55ceec8ae3d0c1c4889fc97
Alicia Santiago <albrown07@mac.com>
2019-03-10 16:54:01 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,7 +51,9 @@ extend Concerns::Findable
   end
 
   def list_genres
-
+    Genre.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |genre, i|
+      puts "#{i}. #{genre.name}"
+     end
   end
 
   def list_songs_by_artist
