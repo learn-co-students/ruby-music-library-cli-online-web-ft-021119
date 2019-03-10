a8be031304330dcadcbc1e5c58f81f2cb900fcb0
Alicia Santiago <albrown07@mac.com>
2019-03-10 15:43:31 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -39,7 +39,7 @@ extend Concerns::Findable
   end
 
   def list_songs
-    Song.all.sort {|a, b| a.name <=> b.name}
+    Song.all.sort {|a, b| a.name <=> b.name}.each_with_index
     # binding.pry
   end
 
