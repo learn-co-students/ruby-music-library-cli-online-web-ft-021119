e6f36835a7aa2587478ff9dc78f1311cecbb1b55
Alicia Santiago <albrown07@mac.com>
2019-03-10 17:00:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -81,9 +81,9 @@ extend Concerns::Findable
   def play_song
     puts "Which song number would you like to play:"
     input = gets.chomp.to_i
+      if (1..Song.)
+      song = Song.all.sort {|a, b| a.name <=> b.name}[input - 1]
 
-    song = Song.all.sort {|a, b| a.name <=> b.name}[input - 1]
-    
   end
 
 end
