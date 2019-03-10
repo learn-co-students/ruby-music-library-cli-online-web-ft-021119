1718bcdad7c45e659b7c093468d9d52ae3bfa40c
Alicia Santiago <albrown07@mac.com>
2019-03-10 16:58:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -73,7 +73,7 @@ extend Concerns::Findable
 
     if genre = Genre.find_by_name(input)
       genre.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, i|
-        puts "#{i}. #{song.name} - #{genre.name}"
+        puts "#{i}. #{song.artist.name} - #{song.name}"
       end
     end
   end
