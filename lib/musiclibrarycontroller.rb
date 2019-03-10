bd8953314eecf12d85a5d9b6d49e7bf478126cd8
Alicia Santiago <albrown07@mac.com>
2019-03-10 16:58:22 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -68,11 +68,11 @@ extend Concerns::Findable
   end
 
   def list_songs_by_genre
-    puts "Please enter the name of an artist:"
+    puts "Please enter the name of a genre:"
     input = gets.chomp
 
-    if artist = Artist.find_by_name(input)
-      artist.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, i|
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |song, i|
         puts "#{i}. #{song.name} - #{genre.name}"
       end
     end
